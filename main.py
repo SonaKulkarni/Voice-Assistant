2e64d3122140890545f8cda37c527ec988318cba
Sona Kulkarni <sonakulkarnimotihari@gmail.com>
2023-02-03 22:57:04 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,72 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who the heck is' in command:
+        person = command.replace('who the heck is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif 'name' in command:
+        talk('my name is alexa')
+    elif 'age' in command:
+        talk('my age is 20 years old')
+    elif 'how is the weather' in command:
+        talk('its cool')
+    elif 'how are you' in command:
+        talk('i am fine and i hope you are also fine')
+    elif'nice to talk to you' in command:
+        talk('me too and thank you')
+    elif 'bye' in command:
+        talk('bye and talk to you soon')
+    else:
+        talk('Please say the command again.')
+
+
+while True:
+    run_alexa()
